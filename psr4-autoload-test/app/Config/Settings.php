486543600123a734dab86178a5de7d15e9fab1f8
Oceanize <utpal@seazoobd.com>
2022-09-11 15:53:51 +0600

    add psr4 autoload

---
+++ psr4-autoload-test/app/Config/Settings.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Config;
+class Settings{
+    public $name = "";
+    
+    public function __construct($name)
+    {
+        echo "Hi ".$this->name = $name;
+    }
+    public function get_name(){
+
+    }
+
+}
---
+++ psr4-autoload-test/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once('./app/bootstrap.php');
+
+$config = new App\Config\Settings("Utpal Biswas");
+
